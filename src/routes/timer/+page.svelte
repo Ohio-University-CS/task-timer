1b8b5569afb0ce77f12b1a7911ca282c0d6b2c88
Isaac Lanning <il225023@ohio.edu>
2026-03-24 12:00:11 -0400

    you judge performance on commits? i guess i'm doing commits now
    funny how vibe coders will benefit from this by committing way more code and making more commits to delete them when it goes wrong, don't ya think?

---
+++ src/routes/timer/+page.svelte
@@ -0,0 +1,28 @@
+<!-- focused timer -->
+<div class="m-5 outline-dotted rounded">
+	<!-- invisible, for timer elements -->
+	<div class="m-5 grid grid-cols-2 outline-dotted"> 
+		<!-- left -->
+		<div class="m-5 outline-dotted">
+			<div class="m-5 outline-solid rounded">
+				<h1 class="text-4xl">00:00:00</h1>
+			</div>	
+			<div class="m-5 outline-solid flex justify-end">
+				<button>(play)</button>
+				<button>(add time)</button>
+			</div>
+		</div>
+		<!-- right -->
+		<div class=" m-5 outline-dotted">	
+			<div class="m-5 outline-solid rounded">
+				<h1 class="text-2xl">TASK NAME</h1>
+			</div> 
+			<div class="m-5 outline-solid">
+				<h2>3/4/26</h2>
+				<h2>DESC</h2>
+			</div>
+		</div>
+	</div>
+</div>
+
+
