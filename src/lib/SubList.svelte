f179a76a4837a00d41790b1f65f7d72a8dad59c3
Elijah Flader <elijahflader@gmail.com>
2026-04-07 13:46:39 -0400

    Update settings page

--- src/lib/SubList.svelte
+++ src/lib/SubList.svelte
@@ -1,10 +1,15 @@
 <script>
+	import Checkbox from "./Checkbox.svelte";
+
     var {options} = $props();
 
 </script>
 
-<div class="flex flex-col items-center justify-start p-4 bg-white w-2xl">
+<div class="flex flex-col items-center justify-between p-4 bg-white w-full">
     {#each options as option}
-        <div class="p-2 hover:bg-gray-200 cursor-pointer">{option}</div>
+        <div class="flex p-2 hover:bg-gray-200 cursor-pointer w-full border-b-0 border-t-2 border-r-2 border-l-2 last:border-b-2 border-black">
+            <div class="flex p-2 hover:bg-gray-200 cursor-pointer w-full">{option}</div>
+        </div>
+        
     {/each}
 </div>
