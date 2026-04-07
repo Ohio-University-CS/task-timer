70453d449f414565f1ecd4b48740f440b6cf2822
Elijah Flader <elijahflader@gmail.com>
2026-04-07 13:38:30 -0400

    Added light mode to caffeine calculator

--- src/routes/caffeine-calculator/+page.svelte
+++ src/routes/caffeine-calculator/+page.svelte
@@ -47,15 +47,15 @@
 </script>
 
 <!-- Creates the main background color and stuff -->
-<div class="bg-radial-[at_50%_75%] from-[#9a784d] to-[#4a3b28] to-75% w-full min-h-screen flex flex-col items-center justify-center p-4">
+<div class="dark:bg-radial-[at_50%_75%] bg-radial-[at_50%_50%] to-[#c4a484] from-white from-25% dark:from-[#9a784d] dark:to-[#4a3b28] to-75% w-full min-h-screen flex flex-col items-center justify-center p-4">
 
 	<!-- Formats the box that holds all that important stuff -->
 	<div class="w-full max-w-[600px] max-h-[85vh] rounded-2xl overflow-hidden flex flex-col border border-white/10 bg-black/10 backdrop-blur-md mt-20 mb-6 shadow-2xl">
 
 		<!-- Header section: Just shows them what it is and what it does -->
 		<div class="p-5 border-b border-white/10 flex flex-col items-center justify-center text-center">
-			<h1 class="text-xl font-bold text-white flex items-center gap-2">Caffeine Intake Optimizer</h1>
-			<p class="text-stone-300 text-xs mt-0.5">Caffeine intake per task based off of your weight</p>
+			<h1 class="text-xl font-bold dark:text-white text-stone-700 flex items-center gap-2">Caffeine Intake Optimizer</h1>
+			<p class="dark:text-stone-300 text-stone-600 text-xs mt-0.5">Caffeine intake per task based off of your weight</p>
 		</div>
 
 		<!-- Fancy mid section that has the weight drag bar -->
@@ -66,12 +66,12 @@
 
 				<!-- Labels and indicates what the drag bar is currently at -->
 				<div class="flex justify-between items-end">
-					<label for="weight" class="text-[10px] font-bold text-stone-300 uppercase tracking-widest">Body Weight</label>
-					<span class="text-3xl font-bold text-amber-400 leading-none">{weight}<span class="text-xs ml-1 text-stone-400 font-sans">lb</span></span>
+					<label for="weight" class="text-[10px] font-bold dark:text-stone-300 text-stone-600 uppercase tracking-widest">Body Weight</label>
+					<span class="text-3xl font-bold text-amber-500 leading-none">{weight}<span class="text-xs ml-1 text-stone-400 font-sans">lb</span></span>
 				</div>
 
 				<!-- The actual drag bar (is that what this is called? Imma go with it anyway) -->
-				<input id="weight" type="range" min="100" max="300" bind:value={weight} class="w-full h-1.5 bg-white/10 rounded-lg appearance-none cursor-pointer accent-amber-500" />
+				<input id="weight" type="range" min="100" max="300" bind:value={weight} class="w-full h-1.5 dark:bg-white/10 bg-amber-400/70 rounded-lg appearance-none cursor-pointer accent-amber-500" />
 			</div>
 
 			<!-- NEED TO MAKE AN INPUT BOX THAT ALLOWS YOU TO TELL HOW MUCH CAFFEINE YOU NORMALLY TAKE AND CALCULATE BASED OFF OF THAT -->
@@ -79,12 +79,12 @@
 			<!-- Task dropdown: Allows the user to select from their tasks they have -->
 			<div class="space-y-3">
 				<!-- Just the title :) -->
-				<label for="task-select" class="text-[10px] font-bold text-stone-300 uppercase tracking-widest">Task Selector</label>
+				<label for="task-select" class="text-[10px] font-bold dark:text-stone-300 text-stone-600 uppercase tracking-widest">Task Selector</label>
 
 				<!-- This has all of the tasks with their given label and mult -->
 				<!-- It has an ugly blue highlight. Any way to change that? -->
 				<div class="relative">
-					<select id="task-select" bind:value={selectedTask} class="w-full p-3 bg-white/5 border border-white/10 rounded-lg appearance-none focus:outline-none text-white text-sm font-medium transition-all">
+					<select id="task-select" bind:value={selectedTask} class="w-full p-3 bg-white/5 border border-white/10 rounded-lg appearance-none focus:outline-none dark:text-white text-stone-600 text-sm font-medium transition-all">
 						{#each tasks as task}
 							<option value={task.title} class="bg-[#4a3b28] text-white">{task.label} : {task.mult} mg/lb</option>
 						{/each}
@@ -96,18 +96,18 @@
 			<!-- Section that has choice of intake and average amount of caffeine you normally have -->
 			<div class="space-y-3 flex flex-col">
 				<div class="relative w-full">
-					<label for="intake-select" class="text-[10px] font-bold text-stone-300 uppercase tracking-widest">Intake Selector</label>
+					<label for="intake-select" class="text-[10px] font-bold dark:text-stone-300 text-stone-600 uppercase tracking-widest">Intake Selector</label>
 
 					<!-- Allows the user to select their preferred method of intake -->
 					<div class="flex gap-3">
-						<select id="intake-select" bind:value={selectedIntake} class="w-3/4 p-3 bg-white/5 border border-white/10 rounded-lg appearance-none focus:outline-none text-white text-sm font-medium transition-all">
+						<select id="intake-select" bind:value={selectedIntake} class="w-3/4 p-3 bg-white/5 border border-white/10 rounded-lg appearance-none focus:outline-none dark:text-white text-stone-600 text-sm font-medium transition-all">
 							{#each intake as item}
 								<option value={item.title} class="bg-[#4a3b28] text-white">{item.title} : {item.concentration} mg per</option>
 							{/each}
 						</select>
 
 						<!-- Allows the user to input how much they normally drink in a day -->
-						<input type="number" bind:value={avgIntake} class="ml-3 w-full h-12 rounded-lg focus:outline-none bg-white/5 border-white/10 text-white placeholder-white [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="Average Intake per Day">
+						<input type="number" bind:value={avgIntake} class="ml-3 w-full h-12 rounded-lg focus:outline-none bg-white/5 border-white/10 text-white dark:placeholder-white placeholder-stone-600 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none" placeholder="Average Intake per Day">
 					</div>
 				</div>
 			</div>
@@ -118,7 +118,7 @@
 		<div class="bg-black/20 p-6 border-t border-white/10 text-center">
 
 			<!-- Title again x_x -->
-			<div class="text-[10px] font-bold text-stone-400 uppercase tracking-widest mb-1">Recommended Intake</div>
+			<div class="text-[10px] font-bold dark:text-stone-400 text-stone-300 uppercase tracking-widest mb-1">Recommended Intake</div>
 
 			<!-- The actual calculated amount of caffeine needed. I just have a simple weight * mult factor right now -->
 			<div class="text-5xl font-black text-white tracking-tighter">{caffeineNeeded}<span class="text-xl font-light ml-1 text-amber-400/80 uppercase">mg</span></div>
