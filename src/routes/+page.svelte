e9bfeb74e7b4ad04721e262485aef76ad0255700
Elijah Flader <elijahflader@gmail.com>
2026-04-09 10:37:34 -0400

    Change the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,46 @@
 <script>
-	import Button from "$lib/Button.svelte";
-	import TaskList from "$lib/TaskList.svelte";
-	import Timer from "$lib/Timer.svelte";
+  import TypingBar from "$lib/TypingBar.svelte";
+  import Button from "$lib/Button.svelte";
+  import FocusedTimer from "$lib/FocusedTimer.svelte";
+  import UnfocusedTimer from "$lib/UnfocusedTimer.svelte";
 
-	import {addTask, loadTasks} from "$lib/tasks.svelte"
+  let timer = 3000
+  let done = false
 
-	var tasks = $state([])
-	var timer = 2930948
+  function timeAdd() {
+    timer += 10000
+    done = false
+  }
 
-	loadTasks(tasks)
-
-	const addTaskBound = (taskname) => addTask(tasks, taskname)
+  function timeUp() {
+    timer = 0;
+    done = true
+  }
 
 </script>
 
-<!-- Task List -->
-<div class="flex flex-col">
-	<div class="flex flex-row">
-		<Timer {timer}></Timer>
-		<TaskList {tasks} addTask={addTaskBound}></TaskList>
-	</div>
-	<div class="flex justify-end">
-		<a href="/settings">
-			<Button text="Settings"></Button>
-		</a>
-	</div>
-	
+
+
+<!-- figure out basic responsive design (for fonts mainly) in this file -->
+<!-- make "focused task" and "unfocused task" from this page -->
+<!-- make crud buttons for both of them -->
+<!-- make draggable components in a vertical queue -->
+
+<!-- idea for "chain" part of task timer: -->
+<!-- chaining tasktimers to avoid distractions when taking "a quick break" -->
+
+
+<!-- the app is vertical in nature -->
+<div class="flex justify-center columns-1 bg-green-50 w-screen h-screen"> 
+  <!-- pw-3/7 -->
+  {#if done}
+    <h1 class="text-9xl content-center text-center">THE TIMER IS DONE</h1>
+  {/if}
+  <div class="m-3 outline-hidden content-center">
+  <div>
+    <FocusedTimer {timer} {timeAdd} {timeUp} name={"Checkpoint 3"} desc={"idk present again again again or smthn"} dueMonth={4} dueDay={9} dueYear={2026}/>
+  </div>
+  <UnfocusedTimer timer={120000} name={"Stretch"}></UnfocusedTimer>  
+  <UnfocusedTimer timer={3600000} name={""} dueMonth={4} dueDay={8} dueYear={2026}></UnfocusedTimer>  
+  </div>
 </div>
